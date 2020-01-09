b856a839e42aad203aa0143ecc2d0405662aadfa
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-09 04:33:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
+  def initialization(one, two, three)
+    @side_one  = one
+    @side_two = two
+    @side_three = three    
+  end
+  def kind
+    if [@side_one, @side_two, @side_three].any? { |e|e > 0 } && [@side_one, @side_two].sum > @side_three
+      if [@side_one, @side_two, @side_three].all? { |e| e==@side_one }
+        :equilateral
+      elsif @side_one == @side_two || @side_two == @side_three
+        :isosceles
+      else
+        :scalene                
+      end
+    else
+      begin
+        raise TriangleError
+      rescue PartnerError => error
+          puts error.message
+      end
+      
+    end
+    
+  end
+  class TriangleError < StandardError
+    def message
+      "Not a valid triangle"
+      
+    end
+    
+  end
 end
